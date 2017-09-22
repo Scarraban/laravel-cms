daa54c8c90d162ec91d27ec127c28605336aaa85
Sam Butler Thompson <sam@q2q-it.com>
2017-09-22 10:52:40 +0100

    Added Collective HTML resource, changed create form to use new framework format

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,12 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="/posts">
-  {{ csrf_field() }}
-  <input type="text" name="title" placeholder="Enter title"/>
-  <textarea name="content"></textarea>
-  <input type="submit" name="submit" />
-</form>
+{!! Form::open(['route' => 'posts.store']) !!}
+  <div class="form-group">
+    {!! Form::label('title', 'Title') !!}
+    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+  </div>
+  <div class="form-group">
+    {!! Form::label('content', 'Content') !!}
+    {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
+  </div>
+  <div class="form-group">
+    {!! Form::submit('Create Post', ['class' => 'btn btn-primary']) !!}
+  </div>
+{!! Form::close() !!}
 @endsection
 @section('footer')
 
